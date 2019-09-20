152052eac253251672ff5e9d37228b5044a56f21
Nathanael Wettstein <nathanael.wettstein@gmail.com>
2019-09-20 17:18:54 +0200

    Update gitignore

--- scripts/run_suite.sh
+++ scripts/run_suite.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-CMD="robot --console verbose --outputdir /reports /suites"
+CMD="robot --console verbose -i BelimoShowCase --outputdir /reports /suites"
 
 echo ${CMD}
 
